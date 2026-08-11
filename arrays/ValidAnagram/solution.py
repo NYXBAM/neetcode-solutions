021784a868109bcab0aff0953ab1837fcf439c30
NYXBAM <delirium.dev@proton.me>
2026-08-11 02:27:39 -0700

    Anagram solution, common complexity O(N log N) and fast, with O(N) complexity

---
+++ arrays/ValidAnagram/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_sorted = sorted(s)
+        t_sorted = sorted(t)
+        return s_sorted == t_sorted
+
+
+from collections import Counter
+
+
+class FastSolution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return Counter(s) == Counter(t)
