7c2f19178baf94a3a510a5ac309cc16f3c495176
NYXBAM <delirium.dev@proton.me>
2026-08-12 02:09:23 -0700

    add: TwoSum problem solution with O(n^2) and O(n) approaches

---
+++ arrays/TwoSum/solution.py
@@ -0,0 +1,18 @@
+# This solution has complexity O(n2)
+class Solution:
+    def twoSum(nums: list[int], target: int) -> list[int]:
+        i = 0
+        for n in range(len(nums)):
+            for nr in range(i + 1, len(nums)):
+                if nums[n] + nums[nr] == target:
+                    return [n, nr]
+
+    # faster solution with O(1)
+
+    def twoSumOptimized(nums: list[int], target: int) -> list[int]:
+        seen = {}
+        for index, num in enumerate(nums):
+            b = target - num
+            if b in seen:
+                return [seen[b], index]
+            seen[num] = index
