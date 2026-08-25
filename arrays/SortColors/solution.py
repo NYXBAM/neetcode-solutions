f600fb714e6721f51b8daac6190376295c0571bc
NYXBAM <delirium.dev@proton.me>
2026-08-25 00:46:11 -0700

    Add sort colors solution

---
+++ arrays/SortColors/solution.py
@@ -0,0 +1,23 @@
+def sortColors(nums: list[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    low = 0
+    mid = 0
+    high = len(nums) - 1
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[high], nums[mid] = nums[mid], nums[high]
+            high -= 1
+
+
+nms = [1, 0, 1, 2]
+
+print(sortColors(nms))
+print(nms)
